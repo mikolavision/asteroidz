35644f693736d91fbb44ca0067b5bef7a16b8d90
christoferpadilla <christoferpadilla@gmail.com>
2012-05-04 03:15:30 +0000

    Edited the AsteroidzActivity to act as the main menu. Added play button and functioning exit button.

--- src/com/asteroids/game/AsteroidzActivity.java
+++ src/com/asteroids/game/AsteroidzActivity.java
@@ -1,13 +1,30 @@
 package com.asteroids.game;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class AsteroidzActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	super.onCreate(savedInstanceState);
+    	setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    	requestWindowFeature(Window.FEATURE_NO_TITLE);
+    	getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main);
+    }
+    
+    public void playGame(View view){
+    	// load game activity
+    }
+    
+    public void exit(View view){
+    	finish();
+        System.exit(0);
     }
 }
