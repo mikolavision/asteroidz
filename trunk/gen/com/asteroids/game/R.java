fe4706cb7acb46b2f9d481ac3e79a62c6260f84f
christoferpadilla@gmail.com <christoferpadilla@gmail.com@36c4e724-ebe9-e76f-7f91-c0c944a10a4f>
2012-05-05 17:20:24 +0000

    Committing changes from branches/cpadilla/asteroidsGame r14 to trunk

--- trunk/gen/com/asteroids/game/R.java
+++ trunk/gen/com/asteroids/game/R.java
@@ -19,6 +19,7 @@ public final class R {
     public static final class string {
         public static final int Title=0x7f040000;
         public static final int app_name=0x7f040001;
+        public static final int exit=0x7f040003;
         public static final int playButton=0x7f040002;
     }
 }
