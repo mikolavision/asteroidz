97e931b4a5769d31e5699ad5cb6047beba6eb9c0
klcme33@gmail.com <klcme33@gmail.com@36c4e724-ebe9-e76f-7f91-c0c944a10a4f>
2012-05-27 02:37:08 +0000

    merging code for real this time. Added the rest of the classes (empty)

---
+++ branches/kevin/interfaceBranch/src/com/asteroids/game/Asteroid.java
@@ -0,0 +1,23 @@
+package com.asteroids.game;
+
+public class Asteroid extends Drawable implements Flyable {
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean collidesWith(Flyable obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void explode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
