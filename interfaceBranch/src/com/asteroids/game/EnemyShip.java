8cb0c5d5cba42cf44243ea600f2ba214535e9dc5
klcme33 <klcme33@gmail.com>
2012-05-27 02:37:08 +0000

    merging code for real this time. Added the rest of the classes (empty)

---
+++ interfaceBranch/src/com/asteroids/game/EnemyShip.java
@@ -0,0 +1,39 @@
+package com.asteroids.game;
+
+import java.util.ArrayList;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class EnemyShip extends Drawable implements Flyable, Shootable {
+
+	@Override
+	public void Bullet(ArrayList bulletList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void shoot(GL10 gl) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean collidesWith(Flyable obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void explode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
