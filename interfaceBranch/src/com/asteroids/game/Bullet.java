d973d776df334ad53c40626cb23d3f5bf01a0c30
klcme33 <klcme33@gmail.com>
2012-05-28 15:56:16 +0000

    Working on bullets

--- interfaceBranch/src/com/asteroids/game/Bullet.java
+++ interfaceBranch/src/com/asteroids/game/Bullet.java
@@ -1,7 +1,30 @@
 package com.asteroids.game;
 
+import javax.microedition.khronos.opengles.GL10;
+
 public class Bullet extends Drawable implements Flyable {
 
+	public Vector2f position;
+	public Vector2f thrust = new Vector2f(0.00001f, 0.00001f);
+	protected float angle;
+	
+	public Bullet(){
+		super();
+		float coords[] = {
+	            // X, Y, Z
+				0.0f,0.0f,0.0f,
+				0.0f,0.3f,0.0f
+	        };
+		setCoords(coords);
+		setDrawMode(GL10.GL_LINE_STRIP);
+		
+		position = new Vector2f();
+		position.x = 0;
+		position.y = 0;
+		
+		angle = 0;
+	}
+	
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
@@ -23,7 +46,11 @@ public class Bullet extends Drawable implements Flyable {
 	@Override
 	public Vector2f getPosition() {
 		// TODO Auto-generated method stub
-		return null;
+		return position;
+	}
+	
+	public float getAngle(){
+		return angle;
 	}
 
 }
