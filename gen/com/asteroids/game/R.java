8bd80c31e94821efed023d43cf7afb1570c729cc
klcme33 <klcme33@gmail.com>
2012-06-12 17:07:13 +0000

    added sound...maybe

--- gen/com/asteroids/game/R.java
+++ gen/com/asteroids/game/R.java
@@ -16,10 +16,13 @@ public final class R {
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class raw {
+        public static final int sound=0x7f040000;
+    }
     public static final class string {
-        public static final int Title=0x7f040000;
-        public static final int app_name=0x7f040001;
-        public static final int exit=0x7f040003;
-        public static final int playButton=0x7f040002;
+        public static final int Title=0x7f050000;
+        public static final int app_name=0x7f050001;
+        public static final int exit=0x7f050003;
+        public static final int playButton=0x7f050002;
     }
 }
